3c98c2c9939305d7b4a8837f030f917bb2397052
Noam Ross <ross@ecohealthalliance.org>
2023-11-27 22:21:02 -0500

    clean up docs

--- R/get_file_version.R
+++ R/get_file_version.R
@@ -1,23 +1,25 @@
 #' Get in files from versioned repository
 #'
 #' Fetches the contents of a file from a versioned repository - a local git
-#' repository, a GitHub repository, or an S3 bucket. Always fetches the file
-#' to a local cache and returns the path to it.
+#' repository, a GitHub repository, or an S3 bucket. Always fetches the file to
+#' a local cache and returns the path to it.
 #'
 #' @param path Path of a file relative to the git directory or bucket
 #' @param ref A git commit SHA, tag, branch or other [revision
 #'   string](https://git-scm.com/docs/git-rev-parse.html#_specifying_revisions):
-#'   such as "HEAD~1", "main@{2023-02-26 18:30:00}", or "branch@{yesterday}".
-#'   Can also be a [git2r::commit()] object. For S3 buckets, a VersionID.
-#'   Defaults to "HEAD", which also means the latest version in an S3 bucket.
+#'   such as `"HEAD~1"`, `"main@{2023-02-26 18:30:00}"`, or
+#'   `"branch@{yesterday}"`. Can also be a [git2r::commit()] object.  For S3
+#'   buckets, a VersionID. Defaults to "HEAD", which also means the latest
+#'   version in an S3 bucket.
 #' @param repo The repository to get the file from. This can be a local git
-#'  directory, a GitHub repository (URL or "owner/repo" string), or an S3 bucket
-#'  indicated by "s3://bucket-name". Defaults to the current working directory.
+#'   directory, a GitHub repository (URL or `"owner/repo"` string), or an S3
+#'   bucket indicated by "s3://bucket-name". Defaults to the current working
+#'   directory.
 #' @param endpoint,region For S3 buckets, the endpoint and region of the bucket.
-#'   If NULL, the default endpoint and region in local config or environment variables are used.
-#'   (Usually `us-east-1` and `s3.amazonaws.com`.)
+#'   If NULL, the default endpoint and region in local config or environment
+#'   variables are used. (Usually `us-east-1` and `s3.amazonaws.com`.)
 #' @export
-#' @return A [path][fs::path()] to the file in the local cache.
+#' @return A [path][fs::fs_path()] to the file in the local cache.
 get_file_version <- function(path, ref = "HEAD", repo = ".", endpoint = NULL, region = NULL) {
   if (!is_scalar_character(path)) {
     stop("path must be a single string")
--- R/tar_read_version.R
+++ R/tar_read_version.R
@@ -1,31 +1,37 @@
 #' Read a target's value from a git repository
 #'
-#' Reads the content of targets from a git repository. Target metadata and
-#' local target files are extracted into a temporary directory before being
-#' read in by `tar_read()`. For targets of type "file", the files are also
-#' extracted and the paths to the extracted files are returned.
+#' Reads the content of targets from a git repository. Target metadata and local
+#' target files are extracted into a temporary directory before being read in by
+#' `tar_read()`. For targets of type "file", the files are also extracted and
+#' the paths to the extracted files are returned.
 #'
-#' For cloud targets, the target metadata is read from git history and then
-#' this metadata is used to download the target from the cloud. For this to work,
+#' For cloud targets, the target metadata is read from git history and then this
+#' metadata is used to download the target from the cloud. For this to work,
 #' cloud storage must be set up with versioning. Note that targets metadata
-#' includes the bucket, endpoint, and region of a S3-stored target, but you must still
-#' provide an AWS access key and secret as environment variables. If these differ
-#' from the credentials used for your current project or environment, can use
-#' [withr::with_envvar()] to temporarily set the credentials.
+#' includes the bucket, endpoint, and region of a S3-stored target, but you must
+#' still provide an AWS access key and secret as environment variables. If these
+#' differ from the credentials used for your current project or environment, can
+#' use [withr::with_envvar()] to temporarily set the credentials.
 #'
-#' @param name Name of the target. `tar_read_version()` can take a symbol, `tar_read_raw_version()` requires a character.
+#' @param name Name of the target. `tar_read_version()` can take a symbol,
+#'   `tar_read_raw_version()` requires a character.
 #' @param ref A git commit SHA, tag, branch or other [revision
 #'   string](https://git-scm.com/docs/git-rev-parse.html#_specifying_revisions):
-#'   such as "HEAD~1", "main@{2023-02-26 18:30:00}", or "branch@{yesterday}".
-#'   Can also be a [git2r::commit()] object. For S3 buckets, a VersionID.
-#'   Defaults to "HEAD", which also means the latest version in an S3 bucket.
-#' @param store Path to the targets store within the project. Defaults to `"_targets`, or the current project's [store name][targets::tar_path_store()] if `repo = "."`.
+#'   such as `"HEAD~1"`, `"main@{2023-02-26 18:30:00}"`, or
+#'   `"branch@{yesterday}"`. Can also be a [git2r::commit()] object.  For S3
+#'   buckets, a VersionID. Defaults to "HEAD", which also means the latest
+#'   version in an S3 bucket.
+#' @param store Path to the targets store within the project. Defaults to
+#'   `"_targets`, or the current project's [store
+#'   name][targets::tar_path_store()] if `repo = "."`.
 #' @param repo The repository to get the file from. This can be a local git
-#'  directory, a GitHub repository (URL or "owner/repo" string), or an S3 bucket
-#'  indicated by "s3://bucket-name". Defaults to the current working directory.
+#'   directory, a GitHub repository (URL or `"owner/repo"` string), or an S3
+#'   bucket indicated by "s3://bucket-name". Defaults to the current working
+#'   directory.
 #' @export
-#' @return The target's value. If the target is of `format = "file"`, this will be the path to the file in the [relic cache][relic_cache()].
-tar_read_version <- function(name, ref = "HEAD", repo = ".", store = targets::tar_path_store()) {
+#' @return The target's value. If the target is of `format = "file"`, this will
+#'   be the path to the file in the [relic cache][relic_cache()].
+tar_read_version <- function(name, ref = "HEAD", repo = ".", store = NULL) {
   check_installed("targets")
   name <- targets::tar_deparse_language(substitute(name))
   tar_read_raw_version(name, ref, repo, store)
@@ -35,7 +41,7 @@ tar_read_version <- function(name, ref = "HEAD", repo = ".", store = targets::ta
 #' @export
 tar_read_raw_version <- function(name, ref = "HEAD", repo = ".", store = NULL) {
   check_installed("targets")
-  meta <- tar_meta_version(ref = ref, store = store, repo = repo)
+  meta <- tar_meta_version(ref = ref, repo = repo, store = store)
   path_store <- attr(meta, "path_store")
   index <- meta$name == name
   if (!any(index) || sum(index) > 1) {
